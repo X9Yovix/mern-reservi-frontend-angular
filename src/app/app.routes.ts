ce45562ebdd00dd2a3e06076aa6191d0346a0663
Karim Ouazzou <karimouazzou@gmail.com>
2024-05-18 15:54:50 +0100

    azy loading cmp, header, footer, not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home",
+    loadComponent: () => import('./pages/home/home.component')
+      .then((c) => c.HomeComponent)
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: "login",
+    loadComponent: () => import('./pages/login/login.component')
+      .then((c) => c.LoginComponent)
+  },
+  {
+    path: "register",
+    loadComponent: () => import('./pages/register/register.component')
+      .then((c) => c.RegisterComponent)
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  constructor(private router: Router) { }
+
+  isActive(route: string): boolean {
+    return this.router.url === route
+  }
+}
