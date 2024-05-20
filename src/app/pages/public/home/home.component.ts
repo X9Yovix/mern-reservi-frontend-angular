32dd4c431b156a224314ccd2678bf55ce6aa4f68
Karim Ouazzou <karimouazzou@gmail.com>
2024-05-20 02:56:24 +0100

    landing page

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '../../../components/footer/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FooterComponent,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  isAuth = false
+  ngOnInit(): void {
+    if (localStorage.getItem('token')) {
+      this.isAuth = true
+    }
+  }
 }
